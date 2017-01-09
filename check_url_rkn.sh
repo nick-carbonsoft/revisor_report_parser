8790806d85186eac09294faa23a0837e69808419
nick-carbonsoft <n.denisov@carbonsoft.ru>
2017-01-09 15:17:08 +0600

    Исправлено(обработка списка):обработка всего pdf
    Не разбиваем на страницы, а сразу обрабатываем

--- check_url_rkn.sh
+++ check_url_rkn.sh
@@ -3,43 +3,34 @@
 # Установить пакеты
 #platform="$(uname -a | grep -o Ubuntu)"
 #if [ "$platform" == "Ubuntu"]; then
-#	apt-get install poppler-utils pdftk
+#       apt-get install poppler-utils pdftk
 #fi
 
 
-create_pdf() {
-	# Разбить PDF на страницы
-        local INPUT="$1"
-	pdftk "$INPUT" burst
-}
-
-create_txt() {
-	# Передать все страницы для преобразовнания в текст
-	local list_pdf
-	list_pdf="$(find . -name "pg_*.pdf" | sort)"
-	for pdf in pdf_list; do
-		pdftotext -raw "$pdf"
-	done
-}
-
 create_full_report() {
-	local txt_file
-	txt_file="$(find . -name "pg_*.txt" | sort)"
-	for txt in txt_file; do
-		if [ "$txt" == "pg_001.txt" ]; then
-			# Для первой страницы, так как там дата получения отчета
-			cat "$txt" | egrep -o '[0-9]{2} [а-яА-Я]{3}, [0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2}' | sed 1d | sed '$!N;s/\n/ /' > /tmp/datetime
-		else
-			cat "$txt" | egrep -o '[0-9]{2} [а-яА-Я]{3}, [0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2}' | sed '$!N;s/\n/ /' > /tmp/datetime
-		fi
-		# Получаем список URL/доменов
-		cat "$txt" | grep -v "С: *" | tr -d '\r\n'| grep -Eo "http(s?):\/\/[^ \"\(\)\<\>]*," |  cut -d ',' -f1 > /tmp/url
-		# cat "$INPUT" | tr -d '\r\n' | grep -v "С: http(s?):\/\/[^ \"\(\)\<\>]*," | grep -Eo "http(s?):\/\/[^ \"\(\)\<\>]*," |  cut -d ',' -f1 > /tmp/url
-		# Получение списка IP
-		cat "$txt" | sed '/^$/d' | tr -d '\r\n' | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' > /tmp/ip
-	done
+        local txt_file
+        txt_file="$(find . -name "*.txt")"
+        cat "$txt_file" | egrep -o '[0-9]{2} [а-яА-Я]{3}, [0-9]{4} [0-9]{2}:[0-9]{2}:[0-9]{2}' | sed 1d | sed '$!N;s/\n/ /' > /tmp/datetime
+        # Получаем список URL/доменов
+        cat "$txt_file" | grep -v "С: *" | tr -d '\r\n'| grep -Eo "http(s?):\/\/[^ \"\(\)\<\>]*," |  cut -d ',' -f1 > /tmp/url
+        # cat "$INPUT" | tr -d '\r\n' | grep -v "С: http(s?):\/\/[^ \"\(\)\<\>]*," | grep -Eo "http(s?):\/\/[^ \"\(\)\<\>]*," |  cut -d ',' -f1 > /tmp/url
+        # Получение списка IP
+        cat "$txt_file" | sed '/^$/d' | tr -d '\r\n' | grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}' > /tmp/ip
+        rm -f "$txt_file"
 }
 
-# Объединяем все файлы построчно
+main() {
+        local pdf_file="$1"
+        if [ $# -lt 1 ]; then
+                echo "Порядок использования $(basename $0) <file.pdf>"
+                exit 65
+        fi
 
-paste /tmp/datetime /tmp/ip /tmp/url
+        if [ -s "$pdf_file" ]; then
+                pdftotext -raw "$pdf_file"
+                create_full_report
+                paste /tmp/datetime /tmp/ip /tmp/url
+        fi
+}
+
+main "$@"
