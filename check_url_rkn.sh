62cd8ee58eedf570ff929c263481b665956f7aa9
nick <moddingshell@gmail.com>
2017-01-23 12:10:07 +0500

    Исправлено(парсер):изменение столбца для IP адресов

--- check_url_rkn.sh
+++ check_url_rkn.sh
@@ -14,31 +14,30 @@ prepare() {
 get_datetime() {
 	local file="$1"
 	local date_regex='[0-9]{2} [а-яА-Я]{3}, [0-9]{4}  [0-9]{2}:[0-9]{2}:[0-9]{2}'
-	cut -f1-2 "$file" | iconv -f cp1251 | egrep -o "$date_regex" | sed 1d | sed '$!N;s/\n/ /' || break
+	cut -f1-2 "$file" | iconv -f cp1251 | egrep -o "$date_regex" | sed 1d | sed 's/  / /g' | sed '$!N;s/\n/  /' || return 0
 }
 
 get_url_and_domain_list() {
 	local file="$1"
 	local url_regex="http(s?):\/\/[^ \"\(\)\<\>]*"
-	awk -F ";" '{print $6}' "$file" | egrep -o "$url_regex" |  cut -d ',' -f1 || break
+	awk -F ";" '{print $6}' "$file" | egrep -o "$url_regex" |  cut -d ',' -f1 || return 0
 }
 
 get_ip_list() {
 	local file="$1"
 	local ip_regex="([0-9]{1,3}\.){3}[0-9]{1,3}"
-	awk -F "," '{print $6}' "$file" | egrep -o "$ip_regex" || break
+	awk -F "," '{print $7}' "$file" | egrep -o "$ip_regex" || return 0
 }
 
 get_url_redirect() {
 	local file="$1"
-	awk -F ";" '{print $10}' "$file" | iconv -f cp1251 | sed 's/^С: //g' | sed -n '/Адрес перенаправления/,$p' | sed 1d || break
+	awk -F ";" '{print $10}' "$file" | iconv -f cp1251 | sed 's/^С: //g' | sed -n '/Адрес перенаправления/,$p' | sed 1d || return 0
 }
 
 replace_url_redirect() {
 	local file1="$1"
 	local file2="$2"
 	local output="$TMPDIR/output"
-	#[[ -s $file{1,2} ]] || break
 	for file in $file{1,2}; do
 		cat -n "$file" > "$file.enum"
 	done
